5caefdf85f2593fc123be18ef58e067ea58c9ffb
Dennis Crissman <dcrissma@redhat.com>
2015-05-19 09:48:57 -0400

    add publish fields and pojos

---
+++ src/main/java/com/redhat/lightblue/hook/publish/model/Event.java
@@ -0,0 +1,96 @@
+package com.redhat.lightblue.hook.publish.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.redhat.lightblue.metadata.types.DateType;
+
+public class Event {
+
+    private String entityName;
+    private String versionText;
+    private String createdBy;
+    private Date creationDate;
+    private Date lastUpdateDate;
+    private String lastUpdatedBy;
+    private String CRUDOperation;
+    private final List<EventIdentity> identity = new ArrayList<>();
+
+    public String getEntityName() {
+        return entityName;
+    }
+
+    public void setEntityName(String entityName) {
+        this.entityName = entityName;
+    }
+
+    public String getVersionText() {
+        return versionText;
+    }
+
+    public void setVersionText(String versionText) {
+        this.versionText = versionText;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    @JsonFormat(pattern = DateType.DATE_FORMAT_STR)
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @JsonFormat(pattern = DateType.DATE_FORMAT_STR)
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    @JsonProperty("CRUDOperation")
+    public String getCRUDOperation() {
+        return CRUDOperation;
+    }
+
+    public void setCRUDOperation(String CRUDOperation) {
+        this.CRUDOperation = CRUDOperation;
+    }
+
+    public List<EventIdentity> getIdentity() {
+        return identity;
+    }
+
+    public void addIdentities(Collection<EventIdentity> eventIdentities) {
+        identity.addAll(eventIdentities);
+    }
+
+    public void addIdentities(EventIdentity... eventIdentities) {
+        for (EventIdentity id : eventIdentities) {
+            identity.add(id);
+        }
+    }
+
+}
--- src/test/java/com/redhat/lightblue/hook/publish/ITPublishHookTest.java
+++ src/test/java/com/redhat/lightblue/hook/publish/ITPublishHookTest.java
@@ -1,15 +1,26 @@
 package com.redhat.lightblue.hook.publish;
 
+import static com.redhat.lightblue.test.Assert.assertNoDataErrors;
+import static com.redhat.lightblue.test.Assert.assertNoErrors;
 import static com.redhat.lightblue.util.JsonUtils.json;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import java.net.UnknownHostException;
+
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.redhat.lightblue.Response;
+import com.redhat.lightblue.crud.FindRequest;
+import com.redhat.lightblue.crud.InsertionRequest;
 import com.redhat.lightblue.mongo.test.AbstractMongoCRUDTestController;
 
 public class ITPublishHookTest extends AbstractMongoCRUDTestController {
+
+    private static final String PUBLISH_VERSION = "0.0.1-SNAPSHOT";
+    private static final String COUNTRY_VERSION = "0.1.0-SNAPSHOT";
 
     @BeforeClass
     public static void preparePublishHookDatasources() {
@@ -23,14 +34,38 @@ public class ITPublishHookTest extends AbstractMongoCRUDTestController {
     @Override
     protected JsonNode[] getMetadataJsonNodes() throws Exception {
         return new JsonNode[]{
-                json(loadResource("/metadata/publish.json", true))
+                json(loadResource("/metadata/publish.json", true)),
+                json(loadResource("/metadata/country_with_publishhook.json", true))
         };
     }
 
+    @Before
+    public void before() throws UnknownHostException {
+        cleanupMongoCollections("country", "publish");
+    }
+
+    /**
+     * If no trigger is provided, then all actions of the CRUD type will create events.
+     */
     @Test
-    public void test() {
-        assertTrue(true);
-        //TODO Something meaningful.
+    public void testPublishWithDefaultTrigger() throws Exception {
+        Response insertResponse = getLightblueFactory().getMediator().insert(createRequest_FromJsonString(
+                InsertionRequest.class,
+                "{\"entity\":\"country\",\"entityVersion\":\"" + COUNTRY_VERSION + "\",\"data\":["
+                        + "{\"name\":\"United States\",\"iso2Code\":\"123\",\"iso3Code\":\"456\"}"
+                        + "]}"));
+        assertNoErrors(insertResponse);
+        assertNoDataErrors(insertResponse);
+        assertEquals(1, insertResponse.getModifiedCount());
+
+        Response findResponse = getLightblueFactory().getMediator().find(createRequest_FromJsonString(
+                FindRequest.class,
+                "{\"entity\":\"publish\",\"entityVersion\":\"" + PUBLISH_VERSION + "\","
+                        + "\"query\":{\"field\":\"objectType\",\"op\":\"$eq\",\"rvalue\":\"publish\"},"
+                        + "\"projection\": [{\"field\":\"*\",\"include\":true,\"recursive\":true}]}"));
+        assertNoErrors(findResponse);
+        assertNoDataErrors(findResponse);
+        assertEquals(1, findResponse.getMatchCount());
     }
 
 }
