3129c03a2a80bb3cdb56dce429a8c7db604461b4
Dennis Crissman <dcrissma@redhat.com>
2015-09-22 15:19:56 -0400

    fix build

--- publish-hook/src/test/java/com/redhat/lightblue/hook/publish/BasePublishHookTest.java
+++ publish-hook/src/test/java/com/redhat/lightblue/hook/publish/BasePublishHookTest.java
@@ -5,9 +5,8 @@ import static com.redhat.lightblue.test.Assert.assertNoErrors;
 import static org.junit.Assert.assertEquals;
 
 import java.net.UnknownHostException;
-
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -18,6 +17,7 @@ import com.redhat.lightblue.crud.FindRequest;
 import com.redhat.lightblue.crud.InsertionRequest;
 import com.redhat.lightblue.crud.UpdateRequest;
 import com.redhat.lightblue.mongo.test.AbstractMongoCRUDTestController;
+import com.redhat.lightblue.test.FakeClientIdentification;
 
 public abstract class BasePublishHookTest extends AbstractMongoCRUDTestController {
 
@@ -37,27 +37,33 @@ public abstract class BasePublishHookTest extends AbstractMongoCRUDTestControlle
     public Set<String> getHooksToRemove() {
         return new HashSet<>();
     }
-    
+
     @Before
     public void before() throws UnknownHostException {
         cleanupMongoCollections("country", "esbEvents");
     }
 
     protected void insertCountry() throws Exception {
+        InsertionRequest insertRequest = createRequest_FromJsonString(
+                InsertionRequest.class,
+                "{\"entity\":\"country\",\"entityVersion\":\"" + COUNTRY_VERSION + "\",\"data\":["
+                + "{\"_id\":\"12312312312\",\"name\":\"United States\",\"iso2Code\":\"123\",\"iso3Code\":\"456\","
+                + "\"optionalField\":[{\"myRandomField\":\"taylor\",\"mySpecificField\":\"swift\"}]}]}");
+        insertRequest.setClientId(new FakeClientIdentification("FakeUser"));
+
         Response insertResponse = getLightblueFactory().getMediator().insert(
-                createRequest_FromJsonString(InsertionRequest.class, "{\"entity\":\"country\",\"entityVersion\":\"" + COUNTRY_VERSION + "\",\"data\":["
-                        + "{\"_id\":\"12312312312\",\"name\":\"United States\",\"iso2Code\":\"123\",\"iso3Code\":\"456\","
-                        + "\"optionalField\":[{\"myRandomField\":\"taylor\",\"mySpecificField\":\"swift\"}]}]}"));
+                insertRequest);
         assertNoErrors(insertResponse);
         assertNoDataErrors(insertResponse);
         assertEquals(1, insertResponse.getModifiedCount());
     }
 
     protected void verifyEvent(int expectedEvents, String query, String expectedIdentityFields, String expectedFields, String operation) throws Exception {
+        FindRequest findRequest = createRequest_FromJsonString(FindRequest.class, "{\"entity\":\"esbEvents\",\"entityVersion\":\"" + ESB_EVENTS_VERSION + "\"," + "\"query\":"
+                + query + "," + "\"projection\": [{\"field\":\"*\",\"include\":true,\"recursive\":true}]}");
+        findRequest.setClientId(new FakeClientIdentification("FakeUser"));
 
-        Response findResponse = getLightblueFactory().getMediator().find(
-                createRequest_FromJsonString(FindRequest.class, "{\"entity\":\"esbEvents\",\"entityVersion\":\"" + ESB_EVENTS_VERSION + "\"," + "\"query\":"
-                        + query + "," + "\"projection\": [{\"field\":\"*\",\"include\":true,\"recursive\":true}]}"));
+        Response findResponse = getLightblueFactory().getMediator().find(findRequest);
         assertNoErrors(findResponse);
         assertNoDataErrors(findResponse);
         assertEquals(expectedEvents, findResponse.getMatchCount());
@@ -66,16 +72,19 @@ public abstract class BasePublishHookTest extends AbstractMongoCRUDTestControlle
         if (expectedEvents > 0) {
             JSONAssert.assertEquals("[{\"identity\":" + expectedIdentityFields
                     + ",\"esbRootEntityName\":\"Country\",\"esbEventEntityName\":\"State\",\"endSystem\":\"nameOfTargetSystem\",\"createdBy\":\"publishHook\",\"version\":\"0.1.0-SNAPSHOT\""
-                    + ",\"status\":\"UNPROCESSED\",\"lastUpdatedBy\":\"publishHook\",\"notes\":null,\"operation\":\"" + operation + "\","
+                    + ",\"status\":\"UNPROCESSED\",\"lastUpdatedBy\":\"publishHook\",\"operation\":\"" + operation + "\","
                     + "\"entityName\":\"country\",\"objectType\":\"esbEvents\"" + expectedFields + "}]", findResponse.getEntityData().toString(), false);
         }
     }
 
     protected void updateCountry(int expectedUpdates, String updates) throws Exception {
+        UpdateRequest updateRequest = createRequest_FromJsonString(UpdateRequest.class, "{ \"entity\":\"country\",\"entityVersion\":\"" + COUNTRY_VERSION + "\","
+                + "\"projection\": [ { \"field\": \"*\", \"include\": \"true\", \"recursive\": true } ],"
+                + " \"query\": { \"field\": \"_id\", \"op\": \"=\", \"rvalue\": \"12312312312\" }," + " \"update\":" + updates + "}");
+        updateRequest.setClientId(new FakeClientIdentification("FakeUser"));
+
         Response updateResponse = getLightblueFactory().getMediator().update(
-                createRequest_FromJsonString(UpdateRequest.class, "{ \"entity\":\"country\",\"entityVersion\":\"" + COUNTRY_VERSION + "\","
-                        + "\"projection\": [ { \"field\": \"*\", \"include\": \"true\", \"recursive\": true } ],"
-                        + " \"query\": { \"field\": \"_id\", \"op\": \"=\", \"rvalue\": \"12312312312\" }," + " \"update\":" + updates + "}"));
+                updateRequest);
         assertNoErrors(updateResponse);
         assertNoDataErrors(updateResponse);
         assertEquals(expectedUpdates, updateResponse.getModifiedCount());
